56d3d7c85c81726c48bc02a2bf5e87059a6b984a
Grace Lovell <bk47466@vh222004.truman.edu>
2025-04-29 11:21:11 -0500

    Server fixes

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -22,7 +22,7 @@ const MyForm = () => {
         };
 
         try {
-            const response = await fetch("http://127.0.0.1:4025/submit", {
+            const response = await fetch("/submit", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(fullData),
